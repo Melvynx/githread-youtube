fc43989f35e1bca47b04c261559766b3abd88a6e
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-07-28 09:42:35 +0700

    commit postlayout

--- src/features/post/PostLayout.tsx
+++ src/features/post/PostLayout.tsx
@@ -22,7 +22,7 @@ export const PostLayout = ({
 }: PostLayoutProps) => {
   return (
     <div className={clsx('flex w-full flex-row items-start p-4', className)}>
-      <Avatar>
+      <Avatar size="default">
         {user.image ? <AvatarImage src={user.image} alt={user.username} /> : null}
         <AvatarFallback>{user.username.slice(0, 2).toUpperCase()}</AvatarFallback>
       </Avatar>
